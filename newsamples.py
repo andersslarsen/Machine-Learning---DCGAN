8f89a2b759a4e72e492b1ead19f7785ec9b14fe7
Henrik Grüner <60741787+henrikgruner@users.noreply.github.com>
2020-11-26 23:54:39 +0100

    Update newsamples.py

--- newsamples.py
+++ newsamples.py
@@ -5,90 +5,61 @@ import load_data
 import torch
 import torchvision.utils as vutils
 import numpy as np
-import argparse
-import time
-import load_data
-
-
-def realnfake(image, image_size, epochs):
-    dataloader = load_data.main((image_size))
-    real = next(iter(dataloader))
-
-    plt.figure(figsize=(64, 64))
-    plt.subplot(1, 2, 1)
-    plt.axis("off")
-    plt.title("Real Images", fontsize=100)
-    plt.imshow(np.transpose(vutils.make_grid(real[0].to(device)[
-               :128], padding=5, normalize=True).cpu(), (1, 2, 0)))
-
-    img = vutils.make_grid(image, padding=2, normalize=True)
-    img = img.detach().numpy()
-    plt.subplot(1, 2, 2)
-    plt.title("Fake Images", fontsize=100)
-    plt.axis("off")
-    plt.imshow(np.transpose(img, (1, 2, 0)))
-
-    plt.savefig('generated_images/RealAndFake' +
-                str(image_size) + "_" + str(epochs) + '.png')
-    plt.close()
-
 
 # Set matplotlib backend i.e. writing file
 # instead of showing to speed up the process
 matplotlib.use('Agg')
-parser = argparse.ArgumentParser(description='Parameters')
-parser.add_argument('-e', type=int, default=50, help="number of epochs")
-parser.add_argument('-img', type=int, default=128,
-                    help="Image size, i.e. 64 or 128")
-parser.add_argument("-g", type=bool, default=True,
-                    help="single image or grid? True = grid")
-parser.add_argument("-r", type=bool, default=False,
-                    help="Generate both real and fake images for comparison")
-args = parser.parse_args()
-
-
-epochs = args.e
-image_grid = args.g
-image_size = args.img
-both = args.r
-ndf = image_size
-ngf = image_size
 
 nc = 3
+ndf = 128
+ngf = 128
 nz = 100
+image_grid = False
 img_list = []
-filepath = 'trained_models/modelG_' + str(image_size)+'_' + str(epochs)
+real = False
 Generator = models.Generator(0, ngf)
+Generator.load_state_dict(torch.load(
+    'modelG_128_55', map_location='cuda'))
 
-try:
-    Generator.load_state_dict(torch.load(
-        filepath, map_location='cpu'))
-except:
-    print("Model does not exist")
-
-device = torch.device('cpu')
-noise = torch.randn(128, 100, 1, 1)
-image = Generator(noise)
-
-
-if(image_grid == False):
-    for i in range(len(image)):
-        img = vutils.make_grid(image[i], padding=2, normalize=True)
-        img = img.detach().numpy()
-        plt.figure(figsize=(128/92, 128/92))
-        plt.axis("off")
-        plt.imshow(np.transpose(img, (1, 2, 0)))
-        plt.savefig(
-            "generated_images/128-55-"+str(i)+".png")
-        plt.close()
-
-if(image_grid == True and both == False):
+#device = torch.device('cuda')
+#noise = torch.randn(128, 100, 1, 1)
+#image = Generator(noise)
+if(real == False):
+	for j in range(100, 500):
+		device = torch.device('cuda')
+		noise = torch.randn(128, 100, 1, 1)
+		image = Generator(noise)
+		if(image_grid == False):
+			for i in range(len(image)):
+				img = vutils.make_grid(image[i], padding=2, normalize=True)
+				img = img.detach().numpy()
+				plt.figure(figsize=(128/96, 128/96), dpi = 96)
+				plt.axis("off")
+				plt.imshow(np.transpose(img, (1, 2, 0)))
+				plt.savefig(
+					"../IS/Inception-Score/data/128-55-"+str(i+j*128)+".png")
+				plt.close()
+if(image_grid == True):
     img = vutils.make_grid(image, padding=2, normalize=True)
     img = img.detach().numpy()
     plt.figure(figsize=(32, 32))
     plt.axis("off")
     plt.imshow(np.transpose(img, (1, 2, 0)))
-    plt.savefig("generated_images/128-55-grid.png")
+    plt.savefig("Training/Gen_images/128-55-grid.png")
     plt.close()
-if(image_grid == True and both == True):
-    realnfake(image, image_size, epochs)
+if(real == True):	
+	for j in range(100):
+		dataloader = load_data.main(128)
+		device = torch.device('cuda')
+		image = next(iter(dataloader))
+		image = image[0].to('cuda')
+		for i in range(128):
+			img = vutils.make_grid(image[i], padding=2, normalize=True)
+			img = img.detach().cpu().numpy()
+			plt.figure(figsize=(128/96, 128/96), dpi = 96)
+			plt.axis("off")
+			plt.imshow(np.transpose(img, (1, 2, 0)))
+			plt.savefig(
+				"new_real/128-55-"+str(i+j*128)+".png")
+			plt.close()
+
